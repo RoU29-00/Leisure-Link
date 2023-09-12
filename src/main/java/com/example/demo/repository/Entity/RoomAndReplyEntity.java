21470f8d83ab09f89685754082392434fa7867ce
kurimototsubasa ttt <tsubasakuri333@gmail.com>
2023-09-12 09:35:48 +0000

    roomAndReplyのEntity作成

--- src/main/java/com/example/demo/repository/Entity/RoomAndReplyEntity.java
+++ src/main/java/com/example/demo/repository/Entity/RoomAndReplyEntity.java
@@ -15,73 +15,23 @@ public class RoomAndReplyEntity {
     @Column(name = "room_id")
     private Long roomId;
 
-    @Column(name = "name")
-    private String name;
 
-    @Column(name = "email")
-    private String email;
-
-    @Column(name = "password")
-    private String password;
-
-    @Column(name = "sex")
-    private String sex;
-
-    @Column(name = "birthday")
-    private Date birthday;
-
-    @Column(name = "line")
-    private String line;
 
     public Long getUserId() {
         return userId;
+    }
+
+    public Long getRoomId() {
+        return roomId;
     }
 
     public void setUserId(Long userId) {
         this.userId = userId;
     }
 
-    public String getName() {
-        return name;
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String sex) {
-        this.sex = sex;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getLine() { return line; }
-
-    public void setLine(String line) { this.line = line; }
+  
 }
