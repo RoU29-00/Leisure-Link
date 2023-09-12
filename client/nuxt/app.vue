3d5c201129516c09ed5da403c6796731dd5afce0
1f10210028 -Rou <s1f102100287@iniad.org>
2023-09-12 08:42:59 +0000

    file1

--- client/nuxt/app.vue
+++ client/nuxt/app.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <nuxt-link to="/">Top</nuxt-link>/ <nuxt-link to="/login">Login</nuxt-link>/
-    <nuxt-link to="/mypage">MyPage</nuxt-link>
+    <nuxt-link to="/">Login</nuxt-link>/ <nuxt-link to="/createAccount">createAccount</nuxt-link>/
+    <nuxt-link to="/mypage">MyPage</nuxt-link>/
+    <nuxt-link to="/home">home</nuxt-link>/
+    <nuxt-link to="/createRecruit">createRecruit</nuxt-link>/
+    <nuxt-link to="/board">board</nuxt-link>
+    <div class="m-5">
+      <!-- <NuxtPage /> -->
+      <div class="btn btn-success">Success</div>
+      <div class="btn btn-danger">Danger</div>
+      <div class="btn btn-warning">Warning</div>
+    </div>
     <NuxtPage />
   </div>
 </template>
---
+++ client/nuxt/pages/board.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+const { $httpClient } = useNuxtApp()
+
+const router = useRouter();
+
+</script>
+
+<template>
+  <h1>掲示板</h1>
+
+  <div>
+    <div><a href="/host"><img src="https://cdn.sotoasobi.net/small_light(progressive=y,q=85,e=gd,jpeghint=y,dw=334,dh=250)/dat/images/leisures/56ea2de7-771b-4a97-881b-a91dd8c2b9a1.jpg"/></a><br><input type="radio" name="date">2023/10/11(水)14:00~18:00<br><input type="radio" name="date">2023/10/12(木)14:00~18:00<br><input type="radio" name="date">2023/10/13(金)14:00~18:00</div>
+    <div><a href="/host"><img src="https://cdn.sotoasobi.net/small_light(progressive=y,q=85,e=gd,jpeghint=y,dw=334,dh=250)/dat/images/leisures/fe850ae9-457a-46b6-a53f-443f2721e8ff.jpg"/></a><br><input type="radio" name="date">2023/10/11(水)14:00~18:00<br><input type="radio" name="date">2023/10/12(木)14:00~18:00<br><input type="radio" name="date">2023/10/13(金)14:00~18:00</div>
+    <div><a href="/host"><img src="https://cdn.sotoasobi.net/small_light(progressive=y,q=85,e=gd,jpeghint=y,dw=334,dh=250)/dat/images/leisures/8c4e6f42-1513-4575-8be5-ab68b9b6fca9.jpg"/></a><br><input type="radio" name="date">2023/10/11(水)14:00~18:00<br><input type="radio" name="date">2023/10/12(木)14:00~18:00<br><input type="radio" name="date">2023/10/13(金)14:00~18:00</div>
+    <div><a href="/host"><img src="https://cdn.sotoasobi.net/small_light(progressive=y,q=85,e=gd,jpeghint=y,dw=334,dh=250)/dat/images/leisures/5720b158-9354-46f2-82fb-68bb724481b7.jpg"/></a><br><input type="radio" name="date">2023/10/11(水)14:00~18:00<br><input type="radio" name="date">2023/10/12(木)14:00~18:00<br><input type="radio" name="date">2023/10/13(金)14:00~18:00</div>
+    <div><a href="/host"><img src="https://cdn.sotoasobi.net/small_light(progressive=y,q=85,e=gd,jpeghint=y,dw=334,dh=250)/dat/images/leisures/63f3112e-03cb-47dc-bca9-9f653fd034c9.jpg"/></a><br><input type="radio" name="date">2023/10/11(水)14:00~18:00<br><input type="radio" name="date">2023/10/12(木)14:00~18:00<br><input type="radio" name="date">2023/10/13(金)14:00~18:00</div>
+  </div>
+</template>
