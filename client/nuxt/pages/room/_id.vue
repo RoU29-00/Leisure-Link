289ab83517e3941a17a113f13ba99dcd389556f8
1f10210028 -Rou <s1f102100287@iniad.org>
2023-09-13 00:44:51 +0000

    5 miss data changes

---
+++ client/nuxt/pages/room/_id.vue
@@ -0,0 +1,11 @@
+<!-- pages/_id.vue -->
+<template>
+  <div>
+    <h1>Dynamic Page</h1>
+    <p>Dynamic ID: {{ $route.params.id }}</p> <!-- 動的なIDを表示 -->
+  </div>
+</template>
+
+<script>
+
+</script>
